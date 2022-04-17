59722ee8d445442bbb711c319d60074a9e38de8f
Emerson Nifa <emersongusta@gmail.com>
2022-04-17 14:57:42 -0300

    mesclando variables

---
+++ mescle-variables.php
@@ -0,0 +1,14 @@
+<?php 
+$nome = 'Emerson Gustavo';
+$sobrenome = 'Nifa';
+$idade = 27;
+
+// $nomeCompleto = $nome. ' ' . $sobrenome; // tipo 1 de contatenação
+// $nomeCompleto = "$nome $sobrenome"; // tipo 2 de contatenação
+// echo $nomeCompleto;
+
+// $frase = "$nome $sobrenome tem $idade de idade";
+// echo $nome;
+
+$nome .= ' '.'Novo Name';
+echo $nome;
